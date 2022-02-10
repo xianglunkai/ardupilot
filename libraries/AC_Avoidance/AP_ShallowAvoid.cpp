1a2a525466085c4e17347a4e10304dbe3e14a002
xianglunkai <1322099375@qq.com>
2022-02-10 09:55:00 +0800

    AP_ShallowAvoid: add shallow avoidance

---
+++ libraries/AC_Avoidance/AP_ShallowAvoid.cpp
@@ -0,0 +1,145 @@
+#include <AP_AHRS/AP_AHRS.h>
+#include <GCS_MAVLink/GCS.h>
+#include <AP_RangeFinder/AP_RangeFinder.h>
+#include <AP_RangeFinder/AP_RangeFinder_Backend.h>
+
+#include <AP_Math/curve_fitting.h>
+
+#include "AP_ShallowAvoid.h"
+
+// singleton instance
+AP_ShallowAvoid *AP_ShallowAvoid::_singleton;
+namespace AP {
+    AP_ShallowAvoid *shallow_avoid()
+    {
+        return AP_ShallowAvoid::get_singleton();
+    }
+}
+
+extern const AP_HAL::HAL &hal;
+
+const AP_Param::GroupInfo AP_ShallowAvoid::var_info[] = {
+
+    AP_GROUPINFO("SAM_DST", 1, AP_ShallowAvoid, _sample_distance, 10),
+
+    AP_GROUPINFO("PRE_DST", 2, AP_ShallowAvoid, _predict_distance, 10),
+
+    AP_GROUPINFO("ANG_MAX", 3, AP_ShallowAvoid, _max_lean_angle, 75),
+
+    AP_GROUPINFO("DPT_MIN", 4, AP_ShallowAvoid, _min_water_depth, 1.0),
+
+    AP_GROUPINFO("RADIUS_MIN", 5, AP_ShallowAvoid, _min_water_radius, 20),
+
+    AP_GROUPINFO("SLOPE_MIN", 6, AP_ShallowAvoid, _min_water_slope, 1),
+
+    AP_GROUPEND
+};
+
+AP_ShallowAvoid::AP_ShallowAvoid()
+{
+    if (_singleton != nullptr) {
+        AP_HAL::panic("AP_OASonar must be singleton");
+    }
+    _singleton = this;
+}
+
+
+// Return true if shallow detected and can't reach destination 
+bool AP_ShallowAvoid::update(const Location &current_loc,const Location& origin,const Location& destination,const Vector2f &ground_speed_vec,const float dt)
+{
+    // get ground course
+    float ground_course_deg;
+    if (ground_speed_vec.length_squared() < sq(0.2f)) {
+        // with zero ground speed use vehicle's heading
+        ground_course_deg = AP::ahrs().yaw_sensor * 0.01f;
+    } else {
+        ground_course_deg = degrees(ground_speed_vec.angle());
+    }
+
+    // Get current mission velocity derivation angle base on current mission line
+    const float bearing_to_dest = origin.get_bearing_to(destination) * 0.01f;
+    const float lean_angle = wrap_180(ground_course_deg - bearing_to_dest);
+
+    // Convert start and end to offsets from EKF origin
+    Vector2f current_NE,origin_NE,destination_NE;
+    if (!current_loc.get_vector_xy_from_origin_NE(current_NE) ||
+        !origin.get_vector_xy_from_origin_NE(origin_NE) ||
+        !destination.get_vector_xy_from_origin_NE(destination_NE)) {
+        return false;
+    }
+
+    // If vehicle already reach destination,return true
+    float mission_distance = origin.get_distance(destination);
+    if (current_NE == destination_NE || mission_distance < 1e-3f) {
+        return true;
+    }
+    current_NE *= 0.01f;destination_NE *= 0.01f;origin_NE *= 0.01f;
+
+    _last_update_ms = AP_HAL::millis();
+
+    // record sonar data into queue
+    RangeFinder *rangefinder = RangeFinder::get_singleton();
+    if (rangefinder == nullptr || !rangefinder->has_data_orient(ROTATION_PITCH_270) || is_zero(dt)){
+        return false;
+    }
+
+    // Timeout called check
+    if(!is_active()){ 
+        _sample_points.clear();
+        _last_avoid_flag = false;
+    }
+
+    // Special consideration if two waypoints close 
+    const float wp_distance = (destination_NE - origin_NE).length();
+    if(wp_distance <= _min_water_radius && _last_avoid_flag == true){
+        GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "Contious shallow avoidance");
+        _last_avoid_flag = false;
+        return true;
+    }else{
+        _last_avoid_flag = false;
+    }
+    
+    // Push back sensor data into dequeue
+    const bool sensor_healthy = (rangefinder->status_orient(ROTATION_PITCH_270) == RangeFinder::Status::Good);
+    const float water_depth_m = rangefinder->distance_cm_orient(ROTATION_PITCH_270,true) * 0.01f;
+    const float speed = AP::ahrs().groundspeed();
+    std::size_t nw = MAX(_sample_distance / (speed * dt),1.0f/ dt);
+    if(sensor_healthy && fabs(lean_angle) <= _max_lean_angle && speed >= 1.0f){
+        if(_sample_points.size() >= nw){
+            _sample_points.pop_front();
+        }
+        _sample_points.push_back(water_depth_m);
+    }else{
+        _sample_points.clear();
+        return false;
+    }
+ 
+    // Fitting model
+    float error_square = 0.0;
+    std::deque<float> _coef;
+    if(_sample_points.size() < nw){return false;}
+
+    // Use algebra method to solve least square
+    _coef = std::move(LeastSquare(_sample_points,dt,&error_square));
+    
+   // Predict and shallow check
+   const float predict_time = dt * nw + MAX(_predict_distance /speed ,1.0f);
+   const float predict_depth = EvaluatePolynomial(_coef,predict_time);
+   if(predict_depth <= _min_water_depth && _coef[1] <= -tan(radians(_min_water_slope))){
+        _last_avoid_flag = true;
+        GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "Shallow avoidance!");
+        return true;
+   }
+
+    return false;
+}
+
+// true if update has been called recently
+bool AP_ShallowAvoid::is_active() const
+{
+    return ((AP_HAL::millis() - _last_update_ms) < 1000);
+}
+
+
+
+
---
+++ libraries/AC_Avoidance/AP_ShallowAvoid.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <AP_Common/AP_Common.h>
+#include <AP_Common/Location.h>
+#include <AP_HAL/AP_HAL.h>
+#include <AP_Math/AP_Math.h>
+
+#include <deque>
+#include <algorithm>
+
+class AP_ShallowAvoid{
+    public:
+        AP_ShallowAvoid();
+        virtual ~AP_ShallowAvoid() = default;
+
+        /* Do not allow copies */
+        CLASS_NO_COPY(AP_ShallowAvoid);
+
+        // Get singleton instance
+        static AP_ShallowAvoid *get_singleton(){
+            return _singleton;
+        }
+
+        // Paramter group info
+        static const struct AP_Param::GroupInfo var_info[];
+
+
+        // Return true if shallows detected and can not reach destination 
+        // Return false if destination could arrivalable temporarily.
+        bool update(const Location &current_loc,const Location& origin,const Location& destination,const Vector2f &ground_speed_vec,const float dt) WARN_IF_UNUSED;
+    
+    private:
+        static AP_ShallowAvoid *_singleton;
+
+        // true if update has been called recently
+        bool is_active() const;
+
+        // Common paramters
+        AP_Int16 _sample_distance;
+        AP_Int16 _predict_distance;
+        AP_Float _max_lean_angle;
+        AP_Float _min_water_depth;
+        AP_Float _min_water_radius;
+        AP_Float _min_water_slope;
+
+        // Common variables
+        std::deque<float> _sample_points;
+        uint32_t _last_update_ms;       // system time of last call to update
+        bool _last_avoid_flag{false};
+};
+
+namespace AP {
+    AP_ShallowAvoid *shallow_avoid();
+};
