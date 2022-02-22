3ab01efbc78eef38d1e18e3b9860dcf6f7d0f4f6
xianglunkai <1322099375@qq.com>
2022-02-22 19:10:43 +0800

    RPM_Analog: set_pin return value

--- libraries/AP_RPM/RPM_Analog.cpp
+++ libraries/AP_RPM/RPM_Analog.cpp
@@ -17,7 +17,9 @@ AP_RPM_Analog::AP_RPM_Analog(AP_RPM &_ap_rpm, uint8_t _instance, AP_RPM::RPM_Sta
 void AP_RPM_Analog::update(void)
 {
     // this copes with changing the pin at runtime
-    _rpm_pin_analog_source->set_pin(get_pin());
+    if(!_rpm_pin_analog_source->set_pin(get_pin())){
+        return;
+    }
 
     state.rate_rpm = _rpm_pin_analog_source->read_average();
     state.rate_rpm *= ap_rpm._params[state.instance].scaling;
