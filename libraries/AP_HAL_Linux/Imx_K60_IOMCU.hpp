24c821b9e1956375e568c64eb085cfc7b0f082de
xianglunkai <1322099375@qq.com>
2022-02-22 18:49:43 +0800

    AP_HAL_Linux: support imx board

---
+++ libraries/AP_HAL_Linux/Imx_K60_IOMCU.hpp
@@ -0,0 +1,385 @@
+#pragma once
+
+#include <termios.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <poll.h>
+
+namespace Imx_K60{
+
+#define BSKLINK_MSG_HEAD_1    0xDE
+#define BSKLINK_MSG_HEAD_2    0xED
+#define BSKLINK_DEVICE_ID     0x02
+#define BSKLINK_SYS_ID        0x00
+#define BSKLINK_MAX_PAYLOAD_LENGTH 64
+
+#define PACKET_HEADER 0x11
+
+#define PACKET_SEND_ADC_VALUE 0x81
+#define PACEET_RECV_SET_PWM_FREQ 101
+#define PACKET_RECV_SET_PWM_DUTY 102
+#define PACKET_RECV_SET_IO 103
+#define MAX_AI_CHN_NUM 12
+
+struct adc_value_s
+{
+	uint16_t root;           // 0: others 1: ARM 2: rc
+	uint8_t PORT_armed_state;   
+    uint8_t PORT_ems_state;    
+    int16_t PORT_speed;         
+    
+    uint8_t STBD_armed_state;   
+    uint8_t STBD_ems_state;    
+    int16_t STBD_speed;   
+
+    uint16_t AIVal[MAX_AI_CHN_NUM];   
+    uint16_t DIVal;   
+}__attribute__((packed));
+
+
+enum class ADC_Channel:unsigned int{
+	control_root = 0,
+	rpm_left  = 1,
+	rpm_right = 2,
+	temperature  = 3,
+	voltage_battery = 4,
+	current_battery = 5,
+};
+
+struct adc_report_s
+{
+    uint8_t id;
+    float data;
+};
+
+class Imx_IOMCU
+{
+public:
+	int fd;
+
+	Imx_IOMCU()
+	{
+		fd = -1;
+		buf_used = 0;
+	}
+	virtual ~Imx_IOMCU()
+	{
+		if (fd != -1)
+		{
+			close(fd);
+			fd = -1;
+			buf_used = 0;
+			poll_fd.fd = -1;
+		}
+	}
+	int init(const char *uart_name, unsigned int baud = 115200)
+	{
+		fd = open(uart_name, O_RDWR | O_NOCTTY);
+		if (fd < 0) {
+			printf("Message : %s\n", strerror(errno));
+			return fd;
+		}
+		if (set_uart_baudrate(fd, baud) <= 0)
+		{
+			printf("Message : %s\n", strerror(errno));
+			close(fd);
+			fd = -1;
+			return -1;
+		}
+		poll_fd.events = POLLIN;
+		poll_fd.fd = fd;
+		return fd;
+	}
+	bool set_freq(uint32_t chmask, uint32_t freq[12])
+	{
+
+		
+		uint32_t packet[13] = { 0 };
+		char packet2[128];
+		packet[0] = chmask;
+		for (int i = 0; i < 12; i++)
+		{
+			packet[i + 1] = freq[i];
+		}
+
+ 		//set_io(0XED,0XDE);	
+
+		return uart_send_packet(fd, PACEET_RECV_SET_PWM_FREQ, packet, packet2, 13 * 4);
+	}
+	bool set_duty(uint32_t chmask, uint32_t duty[12])
+	{
+		uint32_t packet[13] = { 0 };
+		char packet2[128];
+		packet[0] = chmask;
+		//printf("chmask duty = %0x\n",chmask);
+		for (int i = 0; i < 12; i++)
+		{
+			packet[i + 1] = duty[i];
+		}
+
+		return uart_send_packet(fd, PACKET_RECV_SET_PWM_DUTY, packet, packet2, 13 * 4);
+	}
+		bool set_io(uint32_t chmask,uint32_t packet_io)
+	{
+		uint32_t packet[2] = { 0 };
+		char packet2[4];
+		packet[0] = chmask;
+		packet[1] =packet_io;
+
+		return uart_send_packet(fd, PACKET_RECV_SET_IO, packet, packet2, 2 * 4);
+	}
+
+	bool read_adc(adc_report_s *report,unsigned len){
+		float temperature=0;
+		float voltage_battery;
+	
+		int poll_ret = poll(&poll_fd, 1, 0);
+		if (poll_ret <= 0)
+			return false;
+
+		int read_len = read(fd, &uart_buf[buf_used], 4096 - buf_used);
+		if (read_len <= 0)
+		{
+			return false;
+		}
+		buf_used += read_len;
+
+		uint8_t packet_id;
+		uint8_t packet[4096];
+		uint8_t packet_size;
+		packet[0] = 0;
+		if (recv_packet(uart_buf, &buf_used, &packet_id, packet, &packet_size) == false)
+		{
+			return false;
+		}
+		if (PACKET_SEND_ADC_VALUE != packet_id)
+			return false;
+
+		adc_value_s *padc_value = (adc_value_s *)packet;
+
+		// printf("g_DIGroupVal = %x\r\n",padc_value->DIVal);
+
+		temperature		 = 4096000/padc_value->AIVal[10]-1000;
+		temperature 	 = -0.00004*temperature*temperature+0.2118*temperature-147.53;
+
+		voltage_battery  = padc_value->AIVal[6]*36.3/4096.0+0.3;
+
+		if(len < 1 || len > 6){return false;}
+
+		// manul fill 
+		report[0].data = padc_value->root; 
+		report[0].id = 0;
+		report[1].data = padc_value->PORT_speed; 
+		report[1].id = 1;
+		report[2].data = padc_value->STBD_speed;
+		report[2].id = 2;
+		report[3].data = temperature;
+		report[3].id = 3;
+		report[4].data = voltage_battery; 
+		report[4].id = 4;
+		report[5].data = 0; 
+		report[5].id = 5;
+
+		return true;
+	}
+
+	bool read_adc(int32_t *adc_pc0_data, int32_t *adc_pc1_data)
+	{
+		//int i=0;
+		float temperature=0;
+		float voltage_battery;
+		//float voltage_4_2,voltage_3_3,voltage_5_0;
+		int poll_ret = poll(&poll_fd, 1, 0);
+		if (poll_ret <= 0)
+			return false;
+
+		int read_len = read(fd, &uart_buf[buf_used], 4096 - buf_used);
+		if (read_len <= 0)
+		{
+			return false;
+		}
+		buf_used += read_len;
+
+		uint8_t packet_id;
+		uint8_t packet[4096];
+		uint8_t packet_size;
+		packet[0] = 0;
+		if (recv_packet(uart_buf, &buf_used, &packet_id, packet, &packet_size) == false)
+		{
+			return false;
+		}
+		if (PACKET_SEND_ADC_VALUE != packet_id)
+			return false;
+
+		adc_value_s *padc_value = (adc_value_s *)packet;
+
+		// printf("g_DIGroupVal = %x\r\n",padc_value->DIVal);
+
+		temperature		 = 4096000/padc_value->AIVal[10]-1000;
+		temperature 	 = -0.00004*temperature*temperature+0.2118*temperature-147.53;
+
+		voltage_battery  = padc_value->AIVal[6]*36.3/4096.0+0.3;
+
+		*adc_pc0_data   =  voltage_battery;
+		*adc_pc1_data   =  temperature;
+
+		return true;
+	}
+
+private:
+	uint8_t uart_buf[4096];
+	int buf_used;
+	struct pollfd poll_fd;
+
+	int set_uart_baudrate(const int _fd, unsigned int baud)
+	{
+		int speed;
+		switch (baud) {
+		case 9600:   speed = B9600;   break;
+		case 19200:  speed = B19200;  break;
+		case 38400:  speed = B38400;  break;
+		case 57600:  speed = B57600;  break;
+		case 115200: speed = B115200; break;
+		case 230400: speed = B230400; break;
+		default:
+			return -EINVAL;
+		}
+
+		struct termios uart_config;
+
+		int termios_state;
+
+		tcgetattr(_fd, &uart_config);
+
+		uart_config.c_cflag |= (CLOCAL | CREAD);
+		uart_config.c_cflag &= ~PARENB;
+		uart_config.c_cflag &= ~CSTOPB;
+		uart_config.c_cflag &= ~CSIZE;
+		uart_config.c_cflag |= CS8;
+		uart_config.c_cflag &= ~CRTSCTS;
+
+		uart_config.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+
+		uart_config.c_iflag = 0;
+
+		uart_config.c_oflag = 0;
+
+		uart_config.c_cc[VTIME] = 0;
+		uart_config.c_cc[VMIN] = 1;
+
+		if ((termios_state = cfsetispeed(&uart_config, speed)) < 0) {
+			return 0;
+		}
+
+		if ((termios_state = cfsetospeed(&uart_config, speed)) < 0) {
+			return 0;
+		}
+
+		if ((termios_state = tcsetattr(_fd, TCSANOW, &uart_config)) < 0) {
+			return 0;
+		}
+
+		return 1;
+	}
+
+	bool uart_send_packet(int _fd, uint8_t packet_id, void *buffer, void *buffer2, int size)
+	{
+		if (size <= 0)
+			return false;
+
+		int newbuf_len = size  + 2 + 2 + 2 ;
+		uint8_t *oldbuf = (uint8_t *)buffer;
+		uint8_t *newbuf = (uint8_t *)buffer2;
+		newbuf[0] = BSKLINK_MSG_HEAD_1;
+		newbuf[1] = BSKLINK_MSG_HEAD_2;
+		newbuf[2] = BSKLINK_DEVICE_ID;
+		newbuf[3] = BSKLINK_SYS_ID;
+		newbuf[4] = packet_id;
+		newbuf[5] = (uint8_t)size;
+		int i;
+		uint8_t mask = oldbuf[0];
+		for (i = 1; i < size; i++)
+		{
+			mask = mask ^ oldbuf[i];
+		}
+
+		for (i = 0; i < size; i++)
+		{
+			newbuf[i  + 6] = oldbuf[i];
+			
+		}
+
+		int write_bytes = write(_fd, newbuf, newbuf_len);
+		if (write_bytes != newbuf_len)
+			return false;
+
+		return true;
+	}
+
+	bool recv_packet(uint8_t *_uart_buf, int *_buf_used, uint8_t *packet_id, uint8_t *packet, uint8_t *packet_size)
+	{
+		int i;
+		bool first_hit = false;
+		bool found_header = false;
+		for (i = 0; i < *_buf_used; i++)
+		{
+			if (_uart_buf[i] == BSKLINK_MSG_HEAD_1 /*||_uart_buf[i] == BSKLINK_MSG_HEAD_2*/)
+			{
+				if (first_hit == false&&_uart_buf[i] == BSKLINK_MSG_HEAD_1)
+				{
+					first_hit = true;
+					continue; 
+				}
+				// else if(first_hit == true&&_uart_buf[i] == BSKLINK_MSG_HEAD_2)
+				// {
+				// 	found_header = true;
+				// 	i--;
+				// 	break;
+				// }
+			}else if(first_hit == true&&_uart_buf[i] == BSKLINK_MSG_HEAD_2)
+			{
+				found_header = true;
+				i--;
+				break;
+			}
+			else
+			{
+				if (first_hit == true)
+					first_hit = false;
+			}
+		}
+
+		if (found_header)
+		{
+			memmove(_uart_buf, _uart_buf + i, *_buf_used - i);
+			*_buf_used = *_buf_used - i;
+		}
+		else
+		{
+			printf("ACM data miss header!\n");
+			return false;
+		}
+
+		*packet_id = _uart_buf[4];
+		*packet_size = _uart_buf[5];
+		if (*_buf_used < *packet_size  + 2 + 2 + 2 )
+		{
+			return false;
+		}
+		for (i = 0; i < *packet_size; i++)
+		{
+			packet[i] = _uart_buf[6 + i ];
+		}
+
+		memmove(_uart_buf, _uart_buf + *packet_size + 6, *_buf_used - (*packet_size + 6));
+		*_buf_used = *_buf_used - (*packet_size  + 6);
+
+		return true;
+	}
+
+};
+}
---
+++ libraries/AP_HAL_Linux/RCOutput_Imx_IOMCU.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "AP_HAL_Linux.h"
+#include "Imx_IOMCU.hpp"
+
+namespace Linux
+{
+	class RCOutput_Imx_IOMCU : public AP_HAL::RCOutput
+	{
+	public:
+		RCOutput_Imx_IOMCU(Imx_IOMCU *m)
+		{
+			mcu = m;
+		}
+
+		~RCOutput_Imx_IOMCU()
+		{
+
+		}
+		void init() override
+		{
+			set_freq(0xFF, 50);
+		}
+		void set_freq(uint32_t chmask, uint16_t freq_hz) override
+		{
+			if (freq_hz > 400)
+				freq_hz = 400;
+			for (int i = 0; i < 12; i++)
+			{
+				if (chmask & (1u << i))
+				{
+					all_freq[i] = freq_hz;
+				}
+			}
+			mcu->set_freq(chmask, all_freq);
+		}
+		uint16_t get_freq(uint8_t ch) override
+		{
+			return all_freq[ch];
+		}
+		void enable_ch(uint8_t ch) override
+		{
+
+		}
+		void disable_ch(uint8_t ch) override
+		{
+			write(ch, 1);
+		}
+		void write(uint8_t ch, uint16_t period_us) override
+		{
+			if (ch >= 12)
+				return;
+
+			_pulses_buffer[ch] = period_us;
+			_pending_write_mask |= (1U << ch);
+
+			if (!_corking) {
+				_corking = true;
+				push();
+			}
+		}
+		void cork() override
+		{
+			_corking = true;
+		}
+		void push() override
+		{
+			if (!_corking) {
+				return;
+			}
+			_corking = false;
+
+			if (_pending_write_mask == 0)
+				return;
+
+			mcu->set_duty(_pending_write_mask, _pulses_buffer);
+
+			_pending_write_mask = 0;
+		}
+		uint16_t read(uint8_t ch) override
+		{
+			return _pulses_buffer[ch];
+		}
+		void read(uint16_t *period_us, uint8_t len) override
+		{
+			for (int i = 0; i < len; i++)
+			{
+				period_us[i] = read(i);
+			}
+		}
+
+	private:
+		Imx_IOMCU *mcu;
+		uint32_t _pulses_buffer[12];
+		uint32_t all_freq[12];
+		bool _corking = false;
+		uint32_t _pending_write_mask;
+	};
+}
