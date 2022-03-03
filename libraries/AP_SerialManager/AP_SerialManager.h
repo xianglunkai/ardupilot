0a176ca22aa7c2294a8493c1e2368811afb8fb33
xianglunkai <1322099375@qq.com>
2022-03-03 15:28:28 +0800

    AP_SerialManager:merge new serial device

--- libraries/AP_SerialManager/AP_SerialManager.h
+++ libraries/AP_SerialManager/AP_SerialManager.h
@@ -170,6 +170,7 @@ public:
         SerialProtocol_CoDevESC = 41,
         SerialProtocol_MSP_DisplayPort = 42,
         SerialProtocol_MAVLinkHL = 43,
+        SerialProtocol_Batt_KXD_RS485 = 50,
         SerialProtocol_NumProtocols                    // must be the last value
     };
 
