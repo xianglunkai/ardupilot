12be09507a0336581a8e39a214822ca50d185ebf
xianglunkai <1322099375@qq.com>
2022-03-10 16:07:40 +0800

    AP_PRX: delete _

--- libraries/AP_Proximity/AP_Proximity_Params.cpp
+++ libraries/AP_Proximity/AP_Proximity_Params.cpp
@@ -9,14 +9,14 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Values: 0:None,7:LightwareSF40c,2:MAVLink,3:TeraRangerTower,4:RangeFinder,5:RPLidarA2,6:TeraRangerTowerEvo,8:LightwareSF45B,10:SITL,12:AirSimSITL,13:CygbotD1
     // @RebootRequired: True
     // @User: Standard
-    AP_GROUPINFO_FLAGS("_TYPE",   1, AP_Proximity_Params, _type, 0, AP_PARAM_FLAG_ENABLE),
+    AP_GROUPINFO_FLAGS("TYPE",   1, AP_Proximity_Params, _type, 0, AP_PARAM_FLAG_ENABLE),
 
     // @Param: _ORIENT
     // @DisplayName: Proximity sensor orientation
     // @Description: Proximity sensor orientation
     // @Values: 0:Default,1:Upside Down
     // @User: Standard
-    AP_GROUPINFO("_ORIENT", 2, AP_Proximity_Params, _orientation, 0),
+    AP_GROUPINFO("ORIENT", 2, AP_Proximity_Params, _orientation, 0),
 
     // @Param: _YAW_CORR
     // @DisplayName: Proximity sensor yaw correction
@@ -24,7 +24,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: -180 180
     // @User: Standard
-    AP_GROUPINFO("_YAW_CORR", 3, AP_Proximity_Params, _yaw_correction, 0),
+    AP_GROUPINFO("YAW_CORR", 3, AP_Proximity_Params, _yaw_correction, 0),
 
     // @Param: _IGN_ANG1
     // @DisplayName: Proximity sensor ignore angle 1
@@ -32,7 +32,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 360
     // @User: Standard
-    AP_GROUPINFO("_IGN_ANG1", 4, AP_Proximity_Params, _ignore_angle_deg[0], 0),
+    AP_GROUPINFO("IGN_ANG1", 4, AP_Proximity_Params, _ignore_angle_deg[0], 0),
 
     // @Param: _IGN_WID1
     // @DisplayName: Proximity sensor ignore width 1
@@ -40,7 +40,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 127
     // @User: Standard
-    AP_GROUPINFO("_IGN_WID1", 5, AP_Proximity_Params, _ignore_width_deg[0], 0),
+    AP_GROUPINFO("IGN_WID1", 5, AP_Proximity_Params, _ignore_width_deg[0], 0),
 
     // @Param: _IGN_ANG2
     // @DisplayName: Proximity sensor ignore angle 2
@@ -48,7 +48,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 360
     // @User: Standard
-    AP_GROUPINFO("_IGN_ANG2", 6, AP_Proximity_Params, _ignore_angle_deg[1], 0),
+    AP_GROUPINFO("IGN_ANG2", 6, AP_Proximity_Params, _ignore_angle_deg[1], 0),
 
     // @Param: _IGN_WID2
     // @DisplayName: Proximity sensor ignore width 2
@@ -56,7 +56,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 127
     // @User: Standard
-    AP_GROUPINFO("_IGN_WID2", 7, AP_Proximity_Params, _ignore_width_deg[1], 0),
+    AP_GROUPINFO("IGN_WID2", 7, AP_Proximity_Params, _ignore_width_deg[1], 0),
 
     // @Param: _IGN_ANG3
     // @DisplayName: Proximity sensor ignore angle 3
@@ -64,7 +64,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 360
     // @User: Standard
-    AP_GROUPINFO("_IGN_ANG3", 8, AP_Proximity_Params, _ignore_angle_deg[2], 0),
+    AP_GROUPINFO("IGN_ANG3", 8, AP_Proximity_Params, _ignore_angle_deg[2], 0),
 
     // @Param: _IGN_WID3
     // @DisplayName: Proximity sensor ignore width 3
@@ -72,7 +72,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 127
     // @User: Standard
-    AP_GROUPINFO("_IGN_WID3", 9, AP_Proximity_Params, _ignore_width_deg[2], 0),
+    AP_GROUPINFO("IGN_WID3", 9, AP_Proximity_Params, _ignore_width_deg[2], 0),
 
     // @Param: _IGN_ANG4
     // @DisplayName: Proximity sensor ignore angle 4
@@ -80,7 +80,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 360
     // @User: Standard
-    AP_GROUPINFO("_IGN_ANG4", 10, AP_Proximity_Params, _ignore_angle_deg[3], 0),
+    AP_GROUPINFO("IGN_ANG4", 10, AP_Proximity_Params, _ignore_angle_deg[3], 0),
 
     // @Param: _IGN_WID4
     // @DisplayName: Proximity sensor ignore width 4
@@ -88,7 +88,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 127
     // @User: Standard
-    AP_GROUPINFO("_IGN_WID4", 11, AP_Proximity_Params, _ignore_width_deg[3], 0),
+    AP_GROUPINFO("IGN_WID4", 11, AP_Proximity_Params, _ignore_width_deg[3], 0),
 
     // @Param: _IGN_ANG5
     // @DisplayName: Proximity sensor ignore angle 5
@@ -96,7 +96,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 360
     // @User: Standard
-    AP_GROUPINFO("_IGN_ANG5", 12, AP_Proximity_Params, _ignore_angle_deg[4], 0),
+    AP_GROUPINFO("IGN_ANG5", 12, AP_Proximity_Params, _ignore_angle_deg[4], 0),
 
     // @Param: _IGN_WID5
     // @DisplayName: Proximity sensor ignore width 5
@@ -104,7 +104,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 127
     // @User: Standard
-    AP_GROUPINFO("_IGN_WID5", 13, AP_Proximity_Params, _ignore_width_deg[4], 0),
+    AP_GROUPINFO("IGN_WID5", 13, AP_Proximity_Params, _ignore_width_deg[4], 0),
 
     // @Param: _IGN_ANG6
     // @DisplayName: Proximity sensor ignore angle 6
@@ -112,7 +112,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 360
     // @User: Standard
-    AP_GROUPINFO("_IGN_ANG6", 14, AP_Proximity_Params, _ignore_angle_deg[5], 0),
+    AP_GROUPINFO("IGN_ANG6", 14, AP_Proximity_Params, _ignore_angle_deg[5], 0),
 
     // @Param: _IGN_WID6
     // @DisplayName: Proximity sensor ignore width 6
@@ -120,21 +120,21 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: deg
     // @Range: 0 127
     // @User: Standard
-    AP_GROUPINFO("_IGN_WID6", 15, AP_Proximity_Params, _ignore_width_deg[5], 0),
+    AP_GROUPINFO("IGN_WID6", 15, AP_Proximity_Params, _ignore_width_deg[5], 0),
 
     // @Param{Copter}: _IGN_GND
     // @DisplayName: Proximity sensor land detection
     // @Description: Ignore proximity data that is within 1 meter of the ground below the vehicle. This requires a downward facing rangefinder
     // @Values: 0:Disabled, 1:Enabled
     // @User: Standard
-    AP_GROUPINFO_FRAME("_IGN_GND", 16, AP_Proximity_Params, _ign_gnd_enable, 0, AP_PARAM_FRAME_COPTER | AP_PARAM_FRAME_HELI | AP_PARAM_FRAME_TRICOPTER),
+    AP_GROUPINFO_FRAME("IGN_GND", 16, AP_Proximity_Params, _ign_gnd_enable, 0, AP_PARAM_FRAME_COPTER | AP_PARAM_FRAME_HELI | AP_PARAM_FRAME_TRICOPTER),
 
     // @Param: _LOG_RAW
     // @DisplayName: Proximity raw distances log
     // @Description: Set this parameter to one if logging unfiltered(raw) distances from sensor should be enabled
     // @Values: 0:Off, 1:On
     // @User: Advanced
-    AP_GROUPINFO("_LOG_RAW", 17, AP_Proximity_Params, _raw_log_enable, 0),
+    AP_GROUPINFO("LOG_RAW", 17, AP_Proximity_Params, _raw_log_enable, 0),
 
     // @Param: _FILT
     // @DisplayName: Proximity filter cutoff frequency
@@ -142,7 +142,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: Hz
     // @Range: 0 20
     // @User: Advanced
-    AP_GROUPINFO("_FILT", 18, AP_Proximity_Params, _filt_freq, 0.25f),
+    AP_GROUPINFO("FILT", 18, AP_Proximity_Params, _filt_freq, 0.25f),
 
     // @Param: _MIN
     // @DisplayName: Proximity minimum range
@@ -150,7 +150,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: m
     // @Range: 0 500
     // @User: Advanced
-    AP_GROUPINFO("_MIN", 19, AP_Proximity_Params, _min_m, 0.0f),
+    AP_GROUPINFO("MIN", 19, AP_Proximity_Params, _min_m, 0.0f),
 
     // @Param: _MAX
     // @DisplayName: Proximity maximum range
@@ -158,7 +158,7 @@ const AP_Param::GroupInfo AP_Proximity_Params::var_info[] = {
     // @Units: m
     // @Range: 0 500
     // @User: Advanced
-    AP_GROUPINFO("_MAX", 20, AP_Proximity_Params, _max_m, 0.0f),
+    AP_GROUPINFO("MAX", 20, AP_Proximity_Params, _max_m, 0.0f),
 
 
     AP_GROUPEND
