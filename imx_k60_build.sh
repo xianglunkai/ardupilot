83c971afd65d3459a7481f6da3f9fe79d5a70439
xianglunkai <1322099375@qq.com>
2022-04-11 15:33:13 +0800

    new boat ip

--- imx_k60_build.sh
+++ imx_k60_build.sh
@@ -1,3 +1,3 @@
-./waf configure --board imx_k60 --rsync-dest root@192.168.18.101:/opt/apps/ardupilot/
+./waf configure --board imx_k60 --rsync-dest root@192.168.17.101:/opt/apps/ardupilot/
 ./waf --target bin/ardurover --upload
 
