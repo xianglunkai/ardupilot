6c54de9d8a7f2a04bab7625efa7171b9c85da0c1
xianglunkai <1322099375@qq.com>
2022-04-26 17:09:46 +0800

    add support build differnt ip controller

--- imx_k60_build.sh
+++ imx_k60_build.sh
@@ -1,3 +1,13 @@
-./waf configure --board imx_k60 --rsync-dest root@192.168.17.101:/opt/apps/ardupilot/
-./waf --target bin/ardurover --upload
+#!/bin/bash
 
+if [[ $1 = '192.168.17.101' ]];
+then
+  ./waf configure --board imx_k60 --rsync-dest root@192.168.17.101:/opt/apps/ardupilot/
+  ./waf --target bin/ardurover --upload
+elif [[ $1 = '192.168.18.101' ]];
+then
+  ./waf configure --board imx_k60 --rsync-dest root@192.168.18.101:/opt/apps/ardupilot/
+  ./waf --target bin/ardurover --upload
+else
+  echo " ip error."
+fi
