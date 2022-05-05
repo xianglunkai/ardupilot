d0c8819cdcef268445614098fa295d1fe91c36a6
xianglunkai <1322099375@qq.com>
2022-05-05 17:28:13 +0800

    speed_data: add support default constrcutor

--- libraries/AP_Planning/speed_data.h
+++ libraries/AP_Planning/speed_data.h
@@ -7,6 +7,7 @@ namespace planning {
 
 class SpeedPoint {
 public:
+    SpeedPoint():_s(0),_t(0),_v(0),_a(0),_da(0){}
     SpeedPoint(const float s, const float t, 
                const float v, const float a, 
                const float da) : _s(s),_t(t),_v(v),_a(a),_da(da){}
@@ -17,6 +18,12 @@ public:
     float a() const { return _a; }
     float da() const { return _da; }
     
+    void set_s(const float s) { _s = s; }
+    void set_t(const float t) { _t = t; }
+    void set_v(const float v) { _v = v; }
+    void set_a(const float a) { _a = a; }
+    void set_da(const float da) { _da = da; }
+
     static SpeedPoint ToSpeedPoint(const float s, const float t,
                                    const float v = 0, const float a = 0,
                                    const float da = 0)
