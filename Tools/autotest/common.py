efcf1dc224c028f7659bd17bfbdf19c6610d35c0
Peter Barker <pbarker@barker.dropbear.id.au>
2022-06-12 12:18:11 +1000

    autotest: slow down specific tests so they pass

--- Tools/autotest/common.py
+++ Tools/autotest/common.py
@@ -7995,6 +7995,10 @@ Also, ignores heartbeats not from our target system'''
     # this autotest appears to interfere with FixedYawCalibration, no idea why.
     def SITLCompassCalibration(self, compass_count=3, timeout=1000):
         '''Test Fixed Yaw Calibration"'''
+
+        timeout /= 8
+        timeout *= self.speedup
+
         def reset_pos_and_start_magcal(mavproxy, tmask):
             mavproxy.send("sitl_stop\n")
             mavproxy.send("sitl_attitude 0 0 0\n")
