4b2495d4ecc256e2890193d9c4bcc3ef9e3c94df
xianglunkai <1322099375@qq.com>
2022-07-07 13:40:27 +0800

    AP_DPPlanner: draft DPPlanner

---
+++ libraries/AC_Avoidance/AP_DPPlanner.cpp
@@ -0,0 +1,121 @@
+#include "AP_DPPlanner.h"
+#include <AP_Planning/linear_interpolation.h>
+
+const AP_Param::GroupInfo AP_DPPlanner::var_info[] = {
+
+
+  AP_GROUPINFO_FLAGS("ENABLE", 0, AP_DPPlanner, _enable, 0, AP_PARAM_FLAG_ENABLE),
+
+  AP_GROUPINFO("ACC_MAX", 1, AP_DPPlanner, _nfe, 80),
+
+  AP_GROUPINFO("DEC_MAX", 2, AP_DPPlanner, _tf, 16),
+
+  AP_GROUPINFO("SPEED_MAX", 3, AP_DPPlanner, _cruise_speed, 2.0f),
+
+  AP_GROUPINFO("ROB_RADIUS", 4, AP_DPPlanner, _speed_max, 3.0f),
+
+  AP_GROUPINFO("CRUISE_SPD", 5, AP_DPPlanner, _dcceleration_max, 0.3f),
+
+  AP_GROUPINFO("PLN_TIME", 6, AP_DPPlanner, _acceleration_max, 0.3f),
+
+  AP_GROUPINFO("PLN_DIST", 7, AP_DPPlanner, _jerk_max, 10.0f),
+
+  AP_GROUPINFO("OBS_TIME", 8, AP_DPPlanner, _vehicle_radius, 1.0f),
+
+  AP_GROUPINFO("OBS_WGH", 9, AP_DPPlanner, _obstacle_weight, 1.0f),
+
+  AP_GROUPINFO("ACC_WGH", 10, AP_DPPlanner, _obstacle_weight, 1000),
+
+  AP_GROUPINFO("DEC_WGH", 11, AP_DPPlanner, _lateral_weight, 0.1f),
+
+  AP_GROUPINFO("ACC_WGH", 10, AP_DPPlanner, _lateral_change_weight, 0.5f),
+
+  AP_GROUPINFO("DEC_WGH", 11, AP_DPPlanner, _lateral_vel_change_weight, 1.0f),
+
+  AP_GROUPINFO("ACC_WGH", 10, AP_DPPlanner, _longitudinal_vel_weight, 10.0f),
+
+  AP_GROUPINFO("DEC_WGH", 11, AP_DPPlanner, _longitudinal_vel_change_weight, 1.0f),
+
+  AP_GROUPEND
+};
+
+AP_DPPlanner::AP_DPPlanner() 
+{
+    AP_Param::setup_object_defaults(this, var_info);
+
+    _nseg = _nfe / NT;
+    _unit_time = _tf / NT;
+    _time = planning::linspace<NT>(_unit_time, _tf);
+    _station = planning::linspace<NS>(0.0f, _unit_time * _speed_max);
+    _lateral = planning::linspace<NL-1>(0.0f, 1.0f);
+}
+
+
+// run background task to find best cruise speed and update avoidance results
+// returns false if obstacle avoidance is not required
+bool AP_DPPlanner::update(const Location &current_loc, const Location& origin, const Location& destination, const Vector2f &ground_speed_vec, 
+            Location &origin_new, Location &destination_new, float &desired_speed_new, 
+            bool proximity_only)
+{
+    if (!_enable) {
+     return false;
+    }
+
+    Vector2f current_ne, origin_ne, destination_ne, projected_line_unit;
+    // convert location with lat-lng to offsets from ekf orgin
+    if (!current_loc.get_vector_xy_from_origin_NE(current_ne) ||
+        !origin.get_vector_xy_from_origin_NE(origin_ne) ||
+        !destination.get_vector_xy_from_origin_NE(destination_ne)) {
+        // OA is not required
+        return false;
+    }
+}
+
+// get collision cost
+float AP_DPPlanner::get_collision_cost(StateIndex parent_ind, StateIndex current_ind)
+{
+    
+}
+
+// get total cost
+std::pair<float, float> AP_DPPlanner::get_total_cost(StateIndex parent_ind, StateIndex cur_ind)
+{
+
+}
+
+// get lateral offset
+float AP_DPPlanner::get_lateral_offset(const float s, const int16_t l_ind)
+{
+    if (l_ind == NL - 1) {
+        return 0.0f;
+    }
+    const float lb = -_safe_margin;
+    const float ub = _safe_margin;
+    return lb + (ub - lb) * _lateral[l_ind];
+}
+
+// linear polate
+std::vector<planning::Vec2d> AP_DPPlanner::interpolate_linearly(const float parent_s, const int16_t parent_l_ind, const int16_t current_s_ind, const int16_t current_l_ind)
+{
+    std::vector<planning::Vec2d> result(_nseg);
+    float pl = _state.start_l;
+    float ps = _state.start_s;
+    if (parent_l_ind >= 0) {
+        ps = parent_s;
+        pl = get_lateral_offset(ps, parent_l_ind);
+    }
+
+    const float cur_s = ps + _station[current_s_ind];
+    const float cur_l = get_lateral_offset(cur_s, current_l_ind);
+
+    const float s_step = _station[current_s_ind] / _nseg;
+    const float l_step = (cur_l - pl) / _nseg;
+
+    for (int16_t i = 0; i < _nseg; i++) {
+        result[i].set_x(ps + i * s_step);
+        result[i].set_y(pl + i * l_step);
+    }
+
+    return result;
+
+}
---
+++ libraries/AC_Avoidance/AP_DPPlanner.h
@@ -0,0 +1,120 @@
+#pragma once
+#include <AP_Common/AP_Common.h>
+#include <AP_Common/Location.h>
+#include <AP_Math/AP_Math.h>
+#include <AP_HAL/AP_HAL.h>
+#include <tuple>
+#include <limits>
+#include <cmath>
+#include <memory>
+#include <AP_Planning/vec2d.h>
+
+class AP_DPPlanner {
+public:
+    AP_DPPlanner();
+
+    CLASS_NO_COPY(AP_DPPlanner);  /* Do not allow copies */
+
+    // send configuration info stored in front end parameters
+    void set_config(float margin_max) { _safe_margin = MAX(margin_max, 0.0f); }
+
+    // run background task to find best cruise speed and update avoidance results
+    // returns false if obstacle avoidance is not required
+    bool update(const Location &current_loc, const Location& origin, const Location& destination, const Vector2f &ground_speed_vec, 
+                Location &origin_new, Location &destination_new, float &desired_speed_new, 
+                bool proximity_only);
+
+    // paramters table
+    static const struct AP_Param::GroupInfo var_info[];
+
+private:
+    // define some constance
+    static const float INF_MAX = std::numeric_limits<float>::max();
+    static const float INF_MIN = std::numeric_limits<float>::min();
+    static const int NT = 5;
+    static const int NS = 7;
+    static const int NL = 10;
+
+    // state node content
+    struct StateCell {
+        float cost = INF_MAX;
+        float current_s = INF_MIN;
+        int16_t parent_s_ind = -1;
+        int16_t parent_l_ind = -1;
+
+        StateCell() = default;
+
+        StateCell(double cost, double cur_s, int parent_s_ind, int parent_l_ind): 
+                 cost(cost), current_s(cur_s),
+                 parent_s_ind(parent_s_ind), parent_l_ind(parent_l_ind) {}
+    };
+
+    // state node index
+    struct StateIndex {
+        int16_t t = -1, s = -1, l = -1;
+        
+        StateIndex() = default;
+
+        StateIndex(int tt, int ss, int ll) : t(tt), s(ss), l(ll) {}
+    };
+
+    // planning start node
+    struct StartState {
+        float start_s = 0;
+        float start_l = 0;
+        float start_theta = 0;
+    };
+
+    // segment number
+    int16_t _nseg;
+    
+    // segment unit (s)
+    float _unit_time;
+
+    // SLT search nodes
+    std::array<float, NT> _time; 
+    std::array<float, NS> _station;
+    std::array<float, NL-1> _lateral;
+
+    // planning initial node
+    StartState _state;
+    StateCell _state_space[NT][NS][NL];
+
+    // get collision cost
+    float get_collision_cost(StateIndex parent_ind, StateIndex current_ind);
+
+    // get total cost
+    std::pair<float, float> get_total_cost(StateIndex parent_ind, StateIndex cur_ind);
+
+    // get lateral offset
+    float get_lateral_offset(const float s, const int16_t l_ind);
+
+    // linear polate
+    std::vector<planning::Vec2d> interpolate_linearly(const float parent_s, const int16_t parent_l_ind, const int16_t current_s_ind, const int16_t current_l_ind);
+
+private:
+
+    // OA common parameters
+    float _safe_margin;              // object avoidance will ignore objects more than this many meters from vehicle
+    
+       
+    // DP parameters
+    AP_Int8 _enable;                                         // enable or disable algorithm
+    AP_Float _nfe;                                           // number of finite elements used to descretize an OCP
+    AP_Float _tf;                                            // time horizon length
+    AP_Float _cruise_speed;                                  // nominal velocity
+    AP_Float _speed_max;                                     // max speed
+    AP_Float _dcceleration_max;                              // must set > 0
+    AP_Float _acceleration_max;                              // must set > 0
+    AP_Float _jerk_max;                                      // M/S^3
+    AP_Float _vehicle_radius;                                // vehicle size
+    AP_Float _obstacle_weight;                               // cost of obstacles, should be set larger to avoid collision with obstacles
+    AP_Float _lateral_weight;                                // lateral cost, the larger the trajectory the closer to reference line
+    AP_Float _lateral_change_weight;                         // lateral change cost dl/ds, penalty for lateral change
+    AP_Float _lateral_vel_change_weight;                     // lateral change cost, dl/dt, penalty for sudden lateral change
+    AP_Float _longitudinal_vel_weight;                       // longitudinal velocity cost, velocity to the nominal velocity
+    AP_Float _longitudinal_vel_change_weight;                // Cost of longitudinal velocity change, ds/dt
+
+    // internal variables used by background thread
+    
+};
