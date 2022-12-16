fbfe3840649e7c94e695dc35b251a2a609df72f4
xianglunkai <1322099375@qq.com>
2022-12-16 08:41:33 +0800

    add 192.168.100.102 board

--- imx_k60_build.sh
+++ imx_k60_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ $1 = '192.168.17.101' ]];
+if [[ $1 = '192.168.100.102' ]];
 then
-  ./waf configure --board imx_k60 --rsync-dest root@192.168.17.101:/opt/apps/ardupilot/
+  ./waf configure --board imx_k60 --rsync-dest root@192.168.100.102:/opt/apps/ardupilot/
   ./waf --target bin/ardurover --upload
 elif [[ $1 = '192.168.18.101' ]];
 then
