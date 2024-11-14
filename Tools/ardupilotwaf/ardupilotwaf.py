5e7986cec5e98c917a9b8ad06d74195c9973c080
xianglunkai <1322099375@qq.com>
2024-11-14 19:22:39 +0800

    ardupilotwaf: add AP_SCU

--- Tools/ardupilotwaf/ardupilotwaf.py
+++ Tools/ardupilotwaf/ardupilotwaf.py
@@ -24,7 +24,8 @@ COMMON_VEHICLE_DEPENDENT_CAN_LIBRARIES = [
     'AP_KDECAN',
     'AP_PiccoloCAN',
     'AP_PiccoloCAN/piccolo_protocol',
-    'AP_ESSA'
+    'AP_ESSA',
+    'AP_SCU'
 ]
 
 COMMON_VEHICLE_DEPENDENT_LIBRARIES = [
